f360defba6d3e4c8fb67f13684012ce1f69c894d
Josefa Parra <josefa.parra@usach.cl>
2024-07-10 16:45:57 -0400

    listo hasta aqui

---
+++ AppViajes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+# Create your models here.
+class Registrarse(models.Model):
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    email= models.EmailField()
+    ciudad= models.CharField(max_length=40)
+                                                      
+class Crear_Viaje(models.Model):
+    nombre_tutor= models.CharField(max_length=40)
+    lugar= models.CharField(max_length=40)
+    dia= models.DateField()
+    hora= models.CharField(max_length=40)
+    ciudad= models.CharField(max_length=40)
+    codigo_viaje= models.CharField(max_length=40)
+
+class Participar(models.Model):
+    codigo_viaje= models.CharField(max_length=40)
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    email= models.EmailField()
